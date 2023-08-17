bccc166bdba551fb4f930eb08d71da31af7fad15
3ay <lefrost.volkoff@yandex.ru>
2023-08-17 17:25:05 +0300

    some fix

--- src/test/java/ChatServerTest.java
+++ src/test/java/ChatServerTest.java
@@ -1,18 +1,13 @@
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentCaptor;
 import ru.netology.homework.ChatServer;
 
 import java.io.IOException;
-import java.io.OutputStream;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
-import static org.mockito.Mockito.*;
 
 public class ChatServerTest {
     @Test
